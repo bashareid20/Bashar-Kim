af557d6ac38f8e4ac9a71bd2a8b5c072badcb462
Bashar EID <117010924+bashareid20@users.noreply.github.com>
2023-05-16 23:56:01 +0300

    iletişim html ve CSS sayfaları
    iletişim html sayfasını yapıp ve css html sayfaları bağladım

--- BilgiAl.php
+++ BilgiAl.php
@@ -5,3 +5,19 @@ print_r($_post);
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kayıt Ol</title>
+    <link rel="stylesheet" href="SingUp.css">
+</head>
+<body>
+
+
+<a href=""></a>
+
+   </body>
+   </html>
